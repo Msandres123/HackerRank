f5477e2d8b452fb820ff610a481bb07067d7f024
Mikael Andres <msandres123@gmail.com>
2022-04-14 12:11:57 -0400

    refactor

--- repeatedString.js
+++ repeatedString.js
@@ -2,13 +2,8 @@ function repeatedString(s, n) {
     // Write your code here
     let arr = s.split("")
     let count = arr.filter((item) => item === "a").length * Math.floor(n/s.length)
-  if (n % s.length !== 0) {
-        for (let i = 0; i < n % s.length; i++) {
-            if(s[i] === "a") count++
-        }
-  }
+    for (let i = 0; i < n % s.length; i++) {if(s[i] === "a") count++}
     return count
-
 }
 
 
