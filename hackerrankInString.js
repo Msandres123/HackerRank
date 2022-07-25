076ed26ead3af73a286e489fc6e0fe436b21d551
Mikael Andres <msandres123@gmail.com>
2022-07-25 10:10:33 -0400

    hackerrankInString refactor

--- hackerrankInString.js
+++ hackerrankInString.js
@@ -1,16 +1,15 @@
 function hackerrankInString(s) {
     // Write your code here
-    let hackArr = ["h", "a", "c", "k", "e", "r", "r", "a", "n", "k"]
+    let hacker = "hackerrank"
     let hackInd = 0
-    let finalArr = []
-    let sArr = s.split("")
-    for (let i = 0; i < sArr.length; i++) {
-        if (sArr[i] === hackArr[hackInd]) {
-            finalArr.push(sArr[i])
+    let finalStr = ""
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === hacker[hackInd]) {
+            finalStr+= hacker[hackInd]
             hackInd++
         }
     }
-    if(finalArr.toString() === hackArr.toString()) {
+    if(finalStr === hacker) {
         return "YES"
     }
     return "NO"
