4885cca812a0e126422c422a577eb32a1239217f
Mikael Andres <msandres123@gmail.com>
2022-09-20 14:19:31 -0400

    caesar_cypher_python

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,21 +1,22 @@
-string = 'abcZ'
+string = 'Always-Look-on-the-Bright-Side-of-Life'
 
 def convert(str, k):
+    k = k%26
     new_str = ""
     for letter in str:
         code = ord(letter)
         if (code >= 97 and code <= 122):
             if code + k > 122:
-                new_str+=chr(97+k-1)
+                new_str+=chr(code-(26-k))
             else:
                 new_str+=chr(code+k)
         elif (code >=65 and code <= 90):
             if code + k > 90:
-                new_str+=chr(65+k-1)
+                new_str+=chr(code-(26-k))
             else:
                 new_str+=chr(code+k)
         else:
             new_str+=letter
     return new_str
 
-print(convert(string, 8))
+print(convert(string, 5))
